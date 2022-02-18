7d8ab45e25c204e2b727b4cb1f26fb5cdbed344c
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-18 11:39:03 +0100

    Added folder dist

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
-<template></template>
+<template>
+  <div></div>
+</template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
   components: {},
