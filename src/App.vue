7598c3f6ee99dc81fc32b966bb04cbddb30805f3
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-18 15:29:56 +0100

    Added components in App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
