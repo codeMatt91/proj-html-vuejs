3c0d397c50393e5d054ac2d28aeaa6cfea3285dc
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-22 12:36:29 +0100

    Added style + function in SliderImg

--- src/components/SliderImg.vue
+++ src/components/SliderImg.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="slider">
     <div class="container">
-      <div class="partners py-4 border-top d-flex">
-        <div class="card-img" v-for="(slider, i) in sliders" :key="i">
+      <div
+        class="partners d-flex justify-content-center py-4 border-top"
+        
+      >
+        <i class="fa fa-angle-left position-relative" @click="next"></i>
+        <div class="card-img" v-for="(slider, i) in filteredSliders" :key="i">
           <figure class="d-flex justify-content-center">
-            <img
+            <img 
               :src="require(`../assets/images/clients_partner_${slider}`)"
               alt="logo partner"
             />
           </figure>
         </div>
+        <i class="fa fa-angle-right position-relative" @click="prev"></i>
       </div>
     </div>
   </div>
@@ -19,6 +24,32 @@
 export default {
   name: "SliderImg",
   props: ["sliders"],
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    next() {
+      if (this.currentIndex === this.filteredSliders.length - 1)
+        this.currentIndex = 0;
+      this.currentIndex += 1;
+    },
+    prev() {
+      if (this.currentIndex === 0)
+        this.currentIndex = this.filteredSliders.length - 1;
+      this.currentIndex -= 1;
+    },
+  },
+  computed: {
+    filteredSliders() {
+      const sliders = this.sliders;
+      return sliders.filter((el, i) => i <= 3);
+    },
+    // setImg(i) {
+    //   return (this.currentIndex = i);
+    // },
+  },
 };
 </script>
 
